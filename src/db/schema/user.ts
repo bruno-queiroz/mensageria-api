af97bae5c77f44cb692752484b9e50a066019c88
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-03-07 08:17:40 -0300

    Add default values

--- src/db/schema/user.ts
+++ src/db/schema/user.ts
@@ -1,10 +1,10 @@
 import { text, pgTable, uuid, timestamp } from "drizzle-orm/pg-core";
 
 export const user = pgTable("user", {
-  id: uuid("id").primaryKey(),
-  name: text("name"),
+  id: uuid("id").primaryKey().defaultRandom(),
+  name: text("name").notNull(),
   photo: text("photo"),
-  email: text("email"),
-  password: text("password"),
-  createdAt: timestamp("created_at"),
+  email: text("email").notNull(),
+  password: text("password").notNull(),
+  createdAt: timestamp("created_at").defaultNow(),
 });
