80f3679653d92d6e0784625ee5c1af79b35d4054
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-03-18 10:48:15 -0300

    Create session table

---
+++ src/db/schema/session.ts
@@ -0,0 +1,10 @@
+import { pgTable, text, timestamp } from "drizzle-orm/pg-core";
+import { user } from "./user";
+
+export const sessions = pgTable("session", {
+  sessionToken: text("sessionToken").notNull().primaryKey(),
+  userId: text("userId")
+    .notNull()
+    .references(() => user.id, { onDelete: "cascade" }),
+  expires: timestamp("expires", { mode: "date" }).notNull(),
+});
