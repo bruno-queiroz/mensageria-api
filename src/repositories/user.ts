e9c6e53394998d65db1639aff7f8fa7ae657795b
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-03-20 16:21:28 -0300

    Create findFriend transaction

--- src/repositories/user.ts
+++ src/repositories/user.ts
@@ -1,5 +1,8 @@
+import { and, eq, like, notInArray } from "drizzle-orm";
 import { db } from "../app";
 import { user } from "../db/schema/user";
+import { friendship } from "../db/schema/friendship";
+import { unionAll } from "drizzle-orm/pg-core";
 
 type NewUser = typeof user.$inferInsert;
 
@@ -9,8 +12,34 @@ export const userRepository = {
       id: user.name,
       name: user.name,
       email: user.email,
-      photo: user.photo,
+      photo: user.image,
       createdAt: user.createdAt,
     });
+  },
+  async findFriend(userId: string, query: string) {
+    const invitee = db
+      .select({ id: user.id })
+      .from(friendship)
+      .innerJoin(user, eq(friendship.user_invitee, user.id))
+      .where(eq(friendship.user_inviter, userId));
+
+    const inviter = db
+      .select({ id: user.id })
+      .from(friendship)
+      .innerJoin(user, eq(friendship.user_inviter, user.id))
+      .where(eq(friendship.user_invitee, userId));
+
+    const userFriendsQuery = unionAll(invitee, inviter);
+
+    return await db
+      .select({
+        id: user.id,
+        name: user.name,
+        photo: user.image,
+      })
+      .from(user)
+      .where(
+        and(like(user.name, query + "%"), notInArray(user.id, userFriendsQuery))
+      );
   },
 };
