294fde08bfbe3bb8835c091d9e6cbb8524d58491
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-03-25 08:01:31 -0300

    Set cookies with session token

--- src/controllers/signUpUser.ts
+++ src/controllers/signUpUser.ts
@@ -4,19 +4,23 @@ import { signUpUserService } from "../services/signUpUser";
 
 export const signUpUserController = async (req: Request, res: Response) => {
   try {
-    const { name, email, password, sessionToken } = SignUpUserSchema.parse(
-      req.body
-    );
-    const newUser = await signUpUserService(
-      { name, email, password },
-      sessionToken
+    const userData = SignUpUserSchema.parse(req.body);
+    const { newUser, expireDate, sessionToken } = await signUpUserService(
+      userData
     );
 
-    res.status(201).json({
-      data: newUser,
-      message: "User signed up successfully.",
-      isOk: true,
-    });
+    res
+      .status(201)
+      .cookie("next-auth.session-token", sessionToken, {
+        httpOnly: true,
+        expires: expireDate,
+        secure: true,
+      })
+      .json({
+        data: newUser,
+        message: "User signed up successfully.",
+        isOk: true,
+      });
   } catch (error) {
     console.error("Error signing up user", error);
 
