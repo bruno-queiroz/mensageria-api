78b5a20c99f41208178e7f7e43160904dcaa5554
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-03-25 10:10:45 -0300

    Add method createUserSession

--- src/repositories/session.ts
+++ src/repositories/session.ts
@@ -2,11 +2,20 @@ import { eq } from "drizzle-orm";
 import { db } from "../app";
 import { session } from "../db/schema/session";
 
+interface CreateUserSession {
+  userId: string;
+  sessionToken: string;
+  expires: Date;
+}
+
 export const sessionRepository = {
   async getUserSession(sessionToken: string) {
     return await db
       .select({ expires: session.expires, userId: session.userId })
       .from(session)
       .where(eq(session.sessionToken, sessionToken));
+  },
+  async createUserSession(newSession: CreateUserSession) {
+    return await db.insert(session).values(newSession);
   },
 };
