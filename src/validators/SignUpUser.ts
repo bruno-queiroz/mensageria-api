63777214d9935f918bdb3735c9ddfdd873e1b3be
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-03-25 07:56:41 -0300

    Remove sessionToken from SignUpUserSchema

--- src/validators/SignUpUser.ts
+++ src/validators/SignUpUser.ts
@@ -5,7 +5,6 @@ export const SignUpUserSchema = z.object({
   email: z.string().email(),
   password: z.string(),
   image: z.string().optional(),
-  sessionToken: z.string().min(30),
 });
 
 type SignUpUserSchemaType = z.infer<typeof SignUpUserSchema>;
