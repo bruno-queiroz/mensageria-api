368c56578be3cc62986da4d8308876fc32fbfee5
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-03-25 10:09:43 -0300

    Create /sign-in route

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { signUpUserController } from "../controllers/signUpUser";
+import { signInUserController } from "../controllers/signInUser";
 
 const router = Router();
 
 router.post("/sign-up", signUpUserController);
-router.post("/sign-in");
+router.post("/sign-in", signInUserController);
 
 export default router;
