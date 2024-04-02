52de274ac1c19e3dc5855c1eb00f41f863ccffa1
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-04-02 10:35:21 -0300

    Change variable name

--- src/utils/setSessionExpireDate.ts
+++ src/utils/setSessionExpireDate.ts
@@ -1,6 +1,6 @@
 export const setSessionExpireDate = ({ days }: { days: number }) => {
-  const oneMonthInMilliseconds = days * 24 * 60 * 60 * 1000;
-  const expireDate = new Date(new Date().getTime() + oneMonthInMilliseconds);
+  const expireDateInMilliseconds = days * 24 * 60 * 60 * 1000;
+  const expireDate = new Date(new Date().getTime() + expireDateInMilliseconds);
 
   return expireDate;
 };
