f1d83773f471755178b32c3d6c48930527c6572e
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-04-03 11:10:04 -0300

    Create send friendship request controller

---
+++ src/controllers/sendFriendshipRequest.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from "express";
+import { FriendshipRequestSchema } from "../validators/FriendshipRequest";
+import { sendFriendshipRequestService } from "../services/sendFriendshipRequest";
+
+export const sendFriendshipRequestController = async (
+  req: Request,
+  res: Response
+) => {
+  try {
+    const friendshipRequest = FriendshipRequestSchema.parse(req.body);
+    await sendFriendshipRequestService(friendshipRequest);
+
+    res
+      .status(201)
+      .json({ message: "Friendship request sent successfully.", isOk: true });
+  } catch (error) {
+    console.error("Error sending friendship request", error);
+
+    res.status(500).json({
+      message: "Something went wrong sending friendship request.",
+      isOk: false,
+    });
+  }
+};
