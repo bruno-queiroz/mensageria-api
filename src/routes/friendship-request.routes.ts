fbb5c5650a9837c204406b55b7146a39f0cd81e5
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-04-04 11:01:49 -0300

    Create get route for getFriendshipRequestController

--- src/routes/friendship-request.routes.ts
+++ src/routes/friendship-request.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { sendFriendshipRequestController } from "../controllers/sendFriendshipRequest";
+import { getFriendshipRequestController } from "../controllers/getFriendshipRequest";
 
 const router = Router();
 
 router.post("/", sendFriendshipRequestController);
+router.get("/", getFriendshipRequestController);
 
 export default router;
