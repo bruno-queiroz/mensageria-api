3bcb417c8501930f7f05010b46ced8575df1a3d1
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-04-04 11:00:26 -0300

    Create method getByUserId

--- src/repositories/friendshipRequest.ts
+++ src/repositories/friendshipRequest.ts
@@ -1,9 +1,29 @@
+import { and, eq } from "drizzle-orm";
 import { db } from "../app";
 import { friendshipRequest } from "../db/schema/friendshipRequest";
 import { FriendshipRequestDto } from "../validators/FriendshipRequest";
+import { user } from "../db/schema/user";
 
 export const friendshipRequestRepository = {
   async create(request: FriendshipRequestDto) {
     return await db.insert(friendshipRequest).values(request);
+  },
+  async getByUserId(userId: string) {
+    console.log("userId", userId);
+    return await db
+      .select({
+        name: user.name,
+        image: user.image,
+        fromUser: friendshipRequest.fromUser,
+        sentAt: friendshipRequest.sentAt,
+      })
+      .from(friendshipRequest)
+      .innerJoin(user, eq(friendshipRequest.fromUser, user.id))
+      .where(
+        and(
+          eq(friendshipRequest.toUser, userId),
+          eq(friendshipRequest.isAccept, false)
+        )
+      );
   },
 };
