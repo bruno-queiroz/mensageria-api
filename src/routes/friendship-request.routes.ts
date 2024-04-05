922ba6443cd92fd2d8878984303a0023ce79a1b5
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-04-05 09:38:52 -0300

    Create /accept route

--- src/routes/friendship-request.routes.ts
+++ src/routes/friendship-request.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import { sendFriendshipRequestController } from "../controllers/sendFriendshipRequest";
 import { getFriendshipRequestController } from "../controllers/getFriendshipRequest";
+import { acceptFriendshipRequestController } from "../controllers/acceptFriendshipRequest";
 
 const router = Router();
 
 router.post("/", sendFriendshipRequestController);
+router.post("/accept", acceptFriendshipRequestController);
+
 router.get("/", getFriendshipRequestController);
 
 export default router;
