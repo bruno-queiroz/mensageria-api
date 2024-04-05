713d3d7cd1cfd2c2e8826c110a9bbe0090885e13
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-04-05 09:37:51 -0300

    Create acceptFriendshipRequestController

---
+++ src/controllers/acceptFriendshipRequest.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from "express";
+import { FriendshipRequestSchema } from "../validators/FriendshipRequest";
+import { acceptFriendshipRequestService } from "../services/acceptFriendshipRequest";
+
+export const acceptFriendshipRequestController = async (
+  req: Request,
+  res: Response
+) => {
+  const request = FriendshipRequestSchema.parse(req.body);
+  await acceptFriendshipRequestService(request);
+
+  try {
+    res.status(200).json({
+      message: "The request was accepted successfully.",
+      isOk: true,
+    });
+  } catch (error) {
+    console.error("Something went wrong when accepting friendship.", error);
+
+    res.status(500).json({
+      message: "Something went wrong when accepting friendship.",
+      isOk: false,
+    });
+  }
+};
