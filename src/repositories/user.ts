b2df5dd79b6169726f878126132e24f417b8cb01
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-04-10 10:57:08 -0300

    Remove findFriend method from userRepository

--- src/repositories/user.ts
+++ src/repositories/user.ts
@@ -31,51 +31,6 @@ export const userRepository = {
 
     return userData;
   },
-  async findFriend(userId: string, query: string) {
-    const invitee = db
-      .select({ id: user.id })
-      .from(friendship)
-      .innerJoin(user, eq(friendship.userInvitee, user.id))
-      .where(eq(friendship.userInviter, userId));
-
-    const inviter = db
-      .select({ id: user.id })
-      .from(friendship)
-      .innerJoin(user, eq(friendship.userInviter, user.id))
-      .where(eq(friendship.userInvitee, userId));
-
-    const userFriendsQuery = unionAll(invitee, inviter);
-
-    const sq = db
-      .select()
-      .from(friendshipRequest)
-      .where(
-        or(
-          eq(friendshipRequest.toUser, userId),
-          eq(friendshipRequest.fromUser, userId)
-        )
-      )
-      .as("sq");
-
-    return await db
-      .select({
-        id: user.id,
-        name: user.name,
-        image: user.image,
-        fromUser: sq.fromUser,
-        toUser: sq.toUser,
-        isAccept: sq.isAccept,
-      })
-      .from(user)
-      .leftJoin(sq, or(eq(sq.toUser, user.id), eq(sq.fromUser, user.id)))
-      .where(
-        and(
-          like(user.name, query + "%"),
-          notInArray(user.id, userFriendsQuery),
-          not(eq(user.id, userId))
-        )
-      );
-  },
   async findUserByEmail(email: string) {
     return await db
       .select({ encryptedPw: user.password, userId: user.id })
