7262bea4a8c593862439e4b901d3a9094c91230a
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-04-10 10:19:59 -0300

    Create private message schema

---
+++ src/db/schema/privateMessage.ts
@@ -0,0 +1,19 @@
+import { pgTable, text, timestamp, boolean, index } from "drizzle-orm/pg-core";
+import { user } from "./user";
+
+export const privateMessage = pgTable(
+  "private_message",
+  {
+    id: text("id").primaryKey(),
+    toUser: text("to_user").references(() => user.id),
+    fromUser: text("from_user").references(() => user.id),
+    message: text("message"),
+    isSeen: boolean("is_seen").default(false),
+    sentAt: timestamp("sent_at").defaultNow(),
+  },
+  (table) => {
+    return {
+      sentAtIndex: index("sent_at_index").on(table.sentAt),
+    };
+  }
+);
