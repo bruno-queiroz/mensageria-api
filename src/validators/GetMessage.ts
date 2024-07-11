2cbd97fea5bfcb89a507beeb91d22eb28d09ac2d
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-07-11 10:35:33 -0300

    Change message schema type

--- src/validators/GetMessage.ts
+++ src/validators/GetMessage.ts
@@ -3,6 +3,7 @@ import { z } from "zod";
 export const GetMessageSchema = z.object({
   toUser: z.string().uuid(),
   fromUser: z.string().uuid(),
+  date: z.string(),
 });
 
 export type GetMessageDto = z.infer<typeof GetMessageSchema>;
