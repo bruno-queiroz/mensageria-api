8940738a86f3a48ad5a2b7c207b0aebce8bddb3a
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-07-15 11:55:26 -0300

    Check if date is truthy

--- src/services/getMessageService.ts
+++ src/services/getMessageService.ts
@@ -5,7 +5,7 @@ import { GetMessageDto } from "../validators/GetMessage";
 export const getMessageService = async (data: GetMessageDto) => {
   const conversationId = getConversationId(data);
 
-  if (data.mode === "default") {
+  if (data.mode === "default" && data.date) {
     return await messageRepository.get(conversationId, data.toUser, data.date);
   }
 
