e888fee37bc5bdea264f2180217462d4daf2ac12
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-07-15 11:19:44 -0300

    Call different methods depending on mode

--- src/services/getMessageService.ts
+++ src/services/getMessageService.ts
@@ -5,5 +5,9 @@ import { GetMessageDto } from "../validators/GetMessage";
 export const getMessageService = async (data: GetMessageDto) => {
   const conversationId = getConversationId(data);
 
-  return await messageRepository.get(conversationId, data.toUser, data.date);
+  if (data.mode === "default") {
+    return await messageRepository.get(conversationId, data.toUser, data.date);
+  }
+
+  return await messageRepository.getNotSeen(conversationId, data.toUser);
 };
