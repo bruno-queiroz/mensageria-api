daae559c8c4d4cafc2b7ced979ebca9282e19863
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-07-15 11:19:56 -0300

    Add mode to schema

--- src/validators/GetMessage.ts
+++ src/validators/GetMessage.ts
@@ -4,6 +4,7 @@ export const GetMessageSchema = z.object({
   toUser: z.string().uuid(),
   fromUser: z.string().uuid(),
   date: z.string(),
+  mode: z.string(),
 });
 
 export type GetMessageDto = z.infer<typeof GetMessageSchema>;
